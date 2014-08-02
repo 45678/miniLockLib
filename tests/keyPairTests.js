17f63635abe1ea752c6be6ba2e375bf0dbb74533
45678 <undefined@undefined>
2014-08-02 13:57:30 -0400

    Replaced tabs with spaces in tests.

--- tests/keyPairTests.js
+++ tests/keyPairTests.js
@@ -6,78 +6,78 @@ var Bobby = window.testFixtures.Bobby
 var T = window.keyPairTests = this
 
 T['Alice’s secret phrase is acceptable to miniLock'] = function(test) {
-	test.ok(miniLockLib.secretPhraseIsAcceptable(Alice.secretPhrase))
-	test.done()
+  test.ok(miniLockLib.secretPhraseIsAcceptable(Alice.secretPhrase))
+  test.done()
 }
 
 T['Bobby’s secret phrase is acceptable to miniLock'] = function(test) {
-	test.ok(miniLockLib.secretPhraseIsAcceptable(Bobby.secretPhrase))
-	test.done()
+  test.ok(miniLockLib.secretPhraseIsAcceptable(Bobby.secretPhrase))
+  test.done()
 }
 
 T['Short secret phrase is unacceptable'] = function(test) {
-	test.same(miniLockLib.secretPhraseIsAcceptable('My password is password'), false)
-	test.done()
+  test.same(miniLockLib.secretPhraseIsAcceptable('My password is password'), false)
+  test.done()
 }
 
 T['Compute Alice’s keys from her secret phrase and email address'] = function(test) {
-	miniLockLib.getKeyPair(Alice.secretPhrase, Alice.emailAddress, function(keys){
-		test.same(Object.keys(keys), ['publicKey', 'secretKey'])
-		test.same(keys.publicKey, Alice.publicKey)
-		test.same(keys.secretKey, Alice.secretKey)
-		test.done()
-	})
+  miniLockLib.getKeyPair(Alice.secretPhrase, Alice.emailAddress, function(keys){
+    test.same(Object.keys(keys), ['publicKey', 'secretKey'])
+    test.same(keys.publicKey, Alice.publicKey)
+    test.same(keys.secretKey, Alice.secretKey)
+    test.done()
+  })
 }
 
 T['Compute Bobby’s keys from his secret phrase and email address'] = function(test) {
-	miniLockLib.getKeyPair(Bobby.secretPhrase, Bobby.emailAddress, function(keys){
-		test.same(Object.keys(keys), ['publicKey', 'secretKey'])
-		test.same(keys.publicKey, Bobby.publicKey)
-		test.same(keys.secretKey, Bobby.secretKey)
-		test.done()
-	})
+  miniLockLib.getKeyPair(Bobby.secretPhrase, Bobby.emailAddress, function(keys){
+    test.same(Object.keys(keys), ['publicKey', 'secretKey'])
+    test.same(keys.publicKey, Bobby.publicKey)
+    test.same(keys.secretKey, Bobby.secretKey)
+    test.done()
+  })
 }
 
 T['Make ID for Alice’s public key'] = function(test) {
-	id = miniLockLib.makeID(Alice.publicKey)
-	test.same(id, Alice.miniLockID)
-	test.done()
+  id = miniLockLib.makeID(Alice.publicKey)
+  test.same(id, Alice.miniLockID)
+  test.done()
 }
 
 T['Make ID for Bobby’s public key'] = function(test) {
-	id = miniLockLib.makeID(Alice.publicKey)
-	test.same(id, Alice.miniLockID)
-	test.done()
+  id = miniLockLib.makeID(Alice.publicKey)
+  test.same(id, Alice.miniLockID)
+  test.done()
 }
 
 T['Can’t make ID for undefined key'] = function(test) {
-	try {
-		miniLockLib.makeID(undefined)
-	}
-	catch (error) {
-		test.same(error, 'miniLockLib.makeID() received undefined public key.', error)
-		test.done()
-	}
+  try {
+    miniLockLib.makeID(undefined)
+  }
+  catch (error) {
+    test.same(error, 'miniLockLib.makeID() received undefined public key.', error)
+    test.done()
+  }
 }
 
 T['Can’t make ID for key that is too short'] = function(test) {
-	try {
-		miniLockLib.makeID(new Uint8Array(16))
-	}
-	catch (error) {
-		test.same(error, 'miniLockLib.makeID() public key parameter was too short.', error)
-		test.done()
-	}
+  try {
+    miniLockLib.makeID(new Uint8Array(16))
+  }
+  catch (error) {
+    test.same(error, 'miniLockLib.makeID() public key parameter was too short.', error)
+    test.done()
+  }
 }
 
 T['Can’t make ID for key that is too long'] = function(test) {
-	try {
-		miniLockLib.makeID(new Uint8Array(64))
-	}
-	catch (error) {
-		test.same(error, 'miniLockLib.makeID() public key parameter was too long.', error)
-		test.done()
-	}
+  try {
+    miniLockLib.makeID(new Uint8Array(64))
+  }
+  catch (error) {
+    test.same(error, 'miniLockLib.makeID() public key parameter was too long.', error)
+    test.done()
+  }
 }
 
 }) // End of top-level function body.
--- tests/operationTests.js
+++ tests/operationTests.js
@@ -5,149 +5,149 @@ var Bobby = window.testFixtures.Bobby
 
 var T = window.operationTests = this
 
-function readFileFixture(name, callback) {
-	var request = new XMLHttpRequest
-	request.open('GET', '/tests/'+name, true)
-	request.responseType = 'blob'
-	request.onreadystatechange = function(event) {
-		if (request.readyState === 4) {
-			request.response.name = name
-			readFileAsArrayBuffer(request.response, callback)
-		}
-	}
-	request.send()
-}
-
-function readFileAsArrayBuffer(file, callback, errorCallback) {
-	var reader = new FileReader()
-	reader.onload = function(readerEvent) {
-		callback(undefined, {
-			name: file.name,
-			size: file.size,
-			data: readerEvent.target.result // (ArrayBuffer)
-		})
-	}
-	reader.onerror = callback
-	reader.readAsArrayBuffer(file)
-}
-
 T['Encrypt private file for Alice'] = function(test) {
-	readFileFixture('basic.txt', function(error, buffer){
-		if (error) return test.done(error)
-		miniLockLib.encrypt({
-			file: buffer,
-			name: 'alice.txt',
-			keys: Alice.keys,
-			miniLockIDs: [Alice.miniLockID],
-			callback: function(error, encrypted) {
-				test.same(Object.keys(encrypted), ['name', 'data', 'senderID'])
-				test.same(encrypted.name, 'alice.txt.minilock')
-				test.ok(encrypted.data)
-				test.same(encrypted.data.size, 962)
-				test.same(encrypted.data.type, 'application/minilock')
-				test.same(encrypted.senderID, Alice.miniLockID)
-				test.done(error)
-			}
-		})
-	})
+  readFileFixture('basic.txt', function(error, buffer){
+    if (error) return test.done(error)
+    miniLockLib.encrypt({
+      file: buffer,
+      name: 'alice.txt',
+      keys: Alice.keys,
+      miniLockIDs: [Alice.miniLockID],
+      callback: function(error, encrypted) {
+        test.same(Object.keys(encrypted), ['name', 'data', 'senderID'])
+        test.same(encrypted.name, 'alice.txt.minilock')
+        test.ok(encrypted.data)
+        test.same(encrypted.data.size, 962)
+        test.same(encrypted.data.type, 'application/minilock')
+        test.same(encrypted.senderID, Alice.miniLockID)
+        test.done(error)
+      }
+    })
+  })
 }
 
 T['Encrypt file for Alice & Bobby'] = function(test) {
-	readFileFixture('basic.txt', function(error, buffer){
-		if (error) return test.done(error)
-		miniLockLib.encrypt({
-			file: buffer,
-			name: 'alice_and_bobby.txt',
-			keys: Alice.keys,
-			miniLockIDs: [Alice.miniLockID, Bobby.miniLockID],
-			callback: function(error, encrypted) {
-				test.same(Object.keys(encrypted), ['name', 'data', 'senderID'])
-				test.same(encrypted.name, 'alice_and_bobby.txt.minilock')
-				test.ok(encrypted.data)
-				test.same(encrypted.data.size, 1508)
-				test.same(encrypted.data.type, 'application/minilock')
-				test.same(encrypted.senderID, Alice.miniLockID)
-				test.done(error)
-			}
-		})
-	})
+  readFileFixture('basic.txt', function(error, buffer){
+    if (error) return test.done(error)
+    miniLockLib.encrypt({
+      file: buffer,
+      name: 'alice_and_bobby.txt',
+      keys: Alice.keys,
+      miniLockIDs: [Alice.miniLockID, Bobby.miniLockID],
+      callback: function(error, encrypted) {
+        test.same(Object.keys(encrypted), ['name', 'data', 'senderID'])
+        test.same(encrypted.name, 'alice_and_bobby.txt.minilock')
+        test.ok(encrypted.data)
+        test.same(encrypted.data.size, 1508)
+        test.same(encrypted.data.type, 'application/minilock')
+        test.same(encrypted.senderID, Alice.miniLockID)
+        test.done(error)
+      }
+    })
+  })
 }
 
 T['Alice can decrypt file her private file'] = function(test) {
-	readFileFixture('alice.txt.minilock', function(error, buffer){
-		if (error) return test.done(error)
-		miniLockLib.decrypt({
-			file: buffer,
-			keys: Alice.keys,
-			callback: function(error, decrypted) {
-				test.ok(decrypted)
-				test.same(Object.keys(decrypted), ['name', 'data', 'senderID'])
-				test.same(decrypted.name, 'basic.txt')
-				test.ok(decrypted.data, '')
-				test.same(decrypted.data.size, 20)
-				test.same(decrypted.senderID, Alice.miniLockID)
-				test.done(error)
-			}
-		})
-	})
+  readFileFixture('alice.txt.minilock', function(error, buffer){
+    if (error) return test.done(error)
+    miniLockLib.decrypt({
+      file: buffer,
+      keys: Alice.keys,
+      callback: function(error, decrypted) {
+        test.ok(decrypted)
+        test.same(Object.keys(decrypted), ['name', 'data', 'senderID'])
+        test.same(decrypted.name, 'basic.txt')
+        test.ok(decrypted.data, '')
+        test.same(decrypted.data.size, 20)
+        test.same(decrypted.senderID, Alice.miniLockID)
+        test.done(error)
+      }
+    })
+  })
 }
 
 T['Alice can decrypt file for Alice & Bobby'] = function(test) {
-	readFileFixture('alice_and_bobby.txt.minilock', function(error, buffer){
-		if (error) return test.done(error)
-		miniLockLib.decrypt({
-			file: buffer,
-			keys: Alice.keys,
-			callback: function(error, decrypted) {
-				test.ok(decrypted)
-				test.same(Object.keys(decrypted), ['name', 'data', 'senderID'])
-				test.same(decrypted.name, 'basic.txt')
-				test.ok(decrypted.data, '')
-				test.same(decrypted.data.size, 20)
-				test.same(decrypted.senderID, Alice.miniLockID)
-				test.done(error)
-			}
-		})
-	})
+  readFileFixture('alice_and_bobby.txt.minilock', function(error, buffer){
+    if (error) return test.done(error)
+    miniLockLib.decrypt({
+      file: buffer,
+      keys: Alice.keys,
+      callback: function(error, decrypted) {
+        test.ok(decrypted)
+        test.same(Object.keys(decrypted), ['name', 'data', 'senderID'])
+        test.same(decrypted.name, 'basic.txt')
+        test.ok(decrypted.data, '')
+        test.same(decrypted.data.size, 20)
+        test.same(decrypted.senderID, Alice.miniLockID)
+        test.done(error)
+      }
+    })
+  })
 }
 
 T['Bobby can decrypt file for Alice & Bobby'] = function(test) {
-	readFileFixture('alice_and_bobby.txt.minilock', function(error, buffer){
-		if (error) return test.done(error)
-		miniLockLib.decrypt({
-			file: buffer,
-			keys: Bobby.keys,
-			callback: function(error, decrypted) {
-				test.ok(decrypted)
-				test.same(Object.keys(decrypted), ['name', 'data', 'senderID'])
-				test.same(decrypted.name, 'basic.txt')
-				test.ok(decrypted.data, '')
-				test.same(decrypted.data.size, 20)
-				test.same(decrypted.senderID, Alice.miniLockID)
-				test.done(error)
-			}
-		})
-	})
+  readFileFixture('alice_and_bobby.txt.minilock', function(error, buffer){
+    if (error) return test.done(error)
+    miniLockLib.decrypt({
+      file: buffer,
+      keys: Bobby.keys,
+      callback: function(error, decrypted) {
+        test.ok(decrypted)
+        test.same(Object.keys(decrypted), ['name', 'data', 'senderID'])
+        test.same(decrypted.name, 'basic.txt')
+        test.ok(decrypted.data, '')
+        test.same(decrypted.data.size, 20)
+        test.same(decrypted.senderID, Alice.miniLockID)
+        test.done(error)
+      }
+    })
+  })
 }
 
 T['Bobby can’t decrypt Alices’s private file'] = function(test) {
-	readFileFixture('alice.txt.minilock', function(error, buffer){
-		if (error) return test.done(error)
-		miniLockLib.decrypt({
-			file: buffer,
-			keys: Bobby.keys,
-			callback: function(error, decrypted) {
-				test.same(error, 'This file does not seem to be encrypted for your miniLock ID. Check that you are logged in with the correct miniLock ID.')
-				test.same(decrypted, undefined)
-				test.done()
-			}
-		})
-	})
+  readFileFixture('alice.txt.minilock', function(error, buffer){
+    if (error) return test.done(error)
+    miniLockLib.decrypt({
+      file: buffer,
+      keys: Bobby.keys,
+      callback: function(error, decrypted) {
+        test.same(error, 'This file does not seem to be encrypted for your miniLock ID. Check that you are logged in with the correct miniLock ID.')
+        test.same(decrypted, undefined)
+        test.done()
+      }
+    })
+  })
 }
 
-// window.URL = window.webkitURL || window.URL		
-// $(document.body).append('<a class="fileSaveLink">Download</a>')
-// $('a.fileSaveLink').attr('download', encrypted.name)
-// $('a.fileSaveLink').attr('href', window.URL.createObjectURL(encrypted.data))
+function readFileFixture(name, callback) {
+  var request = new XMLHttpRequest
+  request.open('GET', '/tests/'+name, true)
+  request.responseType = 'blob'
+  request.onreadystatechange = function(event) {
+    if (request.readyState === 4) {
+      request.response.name = name
+      readFileAsArrayBuffer(request.response, callback)
+    }
+  }
+  request.send()
+}
+
+function readFileAsArrayBuffer(file, callback, errorCallback) {
+  var reader = new FileReader()
+  reader.onload = function(readerEvent) {
+    callback(undefined, {
+      name: file.name,
+      size: file.size,
+      data: readerEvent.target.result // (ArrayBuffer)
+    })
+  }
+  reader.onerror = callback
+  reader.readAsArrayBuffer(file)
+}
 
 }) // End of top-level function body.
+
+// // Paste the follwoing into a test to download a file after an operation.
+// $('body).append('<a id="download_file">Download</a>')
+// $('#download_file').attr('download', encrypted.name)
+// $('#download_file').attr('href', window.URL.createObjectURL(encrypted.data))
